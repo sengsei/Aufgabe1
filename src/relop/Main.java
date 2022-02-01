d6492fc69b490308150db9aede47815ae3cf1970
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-02-01 17:49:04 +0100

    experimental

---
+++ src/relop/Main.java
@@ -0,0 +1,22 @@
+package relop;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        char c = 'a';
+        long l = 100;
+
+        System.out.println("a kleiner b: " + (a < b));
+        System.out.println("a groesser b: " + (a > b));
+        System.out.println("a gleich b: " + (a==b));
+
+        System.out.println("int Wert von char c: " + (int) c);
+
+        System.out.println( l );
+
+    }
+}
