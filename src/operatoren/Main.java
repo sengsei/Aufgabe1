c252897519fbe1a7c30134421c0f01d249f7b8d9
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-02-01 15:38:42 +0100

    solution

---
+++ src/operatoren/Main.java
@@ -0,0 +1,13 @@
+package operatoren;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int summe = a + b;
+        System.out.println(summe);
+        System.out.println("Summe: " + summe);
+
+    }
+}
--- src/task2day2/Main.java
+++
@@ -1,8 +0,0 @@
-package task2day2;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Aufgabe2");
-    }
-}
